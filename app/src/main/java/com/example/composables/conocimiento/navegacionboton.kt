7a0104e9f7f4e482f02c0558e3bdb47fb2c05ab0
Dusty <dcpanes@gmail.com>
2025-10-17 23:15:07 -0300

    elimina importación no usada en navegación con boton

--- app/src/main/java/com/example/composables/conocimiento/navegacionboton.kt
+++ app/src/main/java/com/example/composables/conocimiento/navegacionboton.kt
@@ -4,7 +4,6 @@ import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
